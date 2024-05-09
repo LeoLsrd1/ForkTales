058ca5309d99a8a8da80a17e59786ffc7caf50c5
Léo Lessirard <lessirard.leo@gmail.com>
2024-05-09 17:52:26 +0200

    Change theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.ui.text.google.fonts)
+
+    implementation(libs.androidx.core.splashscreen)
+
     // Navigation
     implementation (libs.androidx.navigation.compose)
     implementation(libs.androidx.material)
--- app/src/main/java/com/ltu/m7019e/forktales/ForkTalesScreen.kt
+++ app/src/main/java/com/ltu/m7019e/forktales/ForkTalesScreen.kt
@@ -11,12 +11,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -28,10 +31,10 @@ import com.ltu.m7019e.forktales.ui.screens.FavoritesScreen
 import com.ltu.m7019e.forktales.ui.screens.HomeScreen
 import com.ltu.m7019e.forktales.ui.screens.ProfileScreen
 
-sealed class ForkTalesNavItem(val route: String, val icon: ImageVector, @StringRes val resourceId: Int) {
-    object Home : ForkTalesNavItem("home", Icons.Default.Home, R.string.home)
-    object Favorites : ForkTalesNavItem("favorites", Icons.Default.Favorite, R.string.favorites)
-    object Profile : ForkTalesNavItem("profile", Icons.Default.Person, R.string.profile)
+sealed class ForkTalesNavItem(val route: String, val iconFilled: ImageVector, val iconOutlined: ImageVector, @StringRes val resourceId: Int) {
+    data object Home : ForkTalesNavItem("home", Icons.Default.Home, Icons.Outlined.Home, R.string.home)
+    data object Favorites : ForkTalesNavItem("favorites", Icons.Default.Favorite, Icons.Outlined.FavoriteBorder, R.string.favorites)
+    data object Profile : ForkTalesNavItem("profile", Icons.Default.Person, Icons.Outlined.Person, R.string.profile)
 }
 
 /**
@@ -48,14 +51,22 @@ fun ForkTalesApp(
     )
     Scaffold(
         bottomBar = {
-            BottomNavigation {
+            BottomNavigation(
+                backgroundColor = MaterialTheme.colorScheme.background,
+            ) {
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentDestination = navBackStackEntry?.destination
                 items.forEach { screen ->
+                    val isSelected = currentDestination?.hierarchy?.any { it.route == screen.route } == true
                     BottomNavigationItem(
-                        icon = { Icon(screen.icon, contentDescription = null) },
+                        icon = {
+                            Icon(
+                                if (isSelected) screen.iconFilled else screen.iconOutlined,
+                                contentDescription = null
+                            )
+                               },
                         label = { Text(text = stringResource(id = screen.resourceId)) },
-                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        selected = isSelected,
                         onClick = {
                             // Pop up to the start destination of the graph to
                             // avoid building up a large stack of destinations
@@ -70,7 +81,9 @@ fun ForkTalesApp(
                                 // Restore state when reselecting a previously selected item
                                 restoreState = true
                             }
-                        }
+                        },
+                        selectedContentColor = MaterialTheme.colorScheme.primary,
+                        unselectedContentColor = MaterialTheme.colorScheme.onBackground,
                     )
                 }
             }
